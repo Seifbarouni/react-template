e904382ee350d74baadf48a613c9a24c07775b45
Seifbarouni <seif.barouni11@gmail.com>
2021-11-11 16:35:01 +0100

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+const App = () => {
+  return (
+    <div className="bg-gray-800 flex h-screen justify-center items-center">
+      <div className="text-red-400 text-3xl bg-white rounded-lg p-6 shadow-lg cursor-pointer transition duration-200 ease-in-out transform hover:-translate-y-1 hover:scale-110">
+        React template with tailwindcss 😄
+      </div>
+    </div>
+  );
+};
+
+export default App;
