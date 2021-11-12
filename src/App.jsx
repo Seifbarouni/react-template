894cc2e9090901ffb622d4780a48c2fef8cc94fe
Seifbarouni <seif.barouni11@gmail.com>
2021-11-12 20:54:52 +0100

    added pages folder + app.jsx design update

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
 const App = () => {
   return (
-    <div className="bg-gray-800 flex h-screen justify-center items-center">
-      <div className="text-red-400 text-3xl bg-white rounded-lg p-6 shadow-lg cursor-pointer transition duration-200 ease-in-out transform hover:-translate-y-1 hover:scale-110">
-        React template with tailwindcss 😄
+    <div className="bg-gradient-to-br from-red-400 to-purple-500 flex h-screen justify-center items-center">
+      <div className="text-3xl bg-white p-6 rounded-lg shadow-lg">
+        React template with tailwindcss
       </div>
+      {/* this glass effect works for all browsers except firefox  */}
+      {/* <div className=" text-3xl  rounded-2xl p-6 shadow-md bg-gray-100 to-transparent  bg-opacity-20 h-80 z-20 backdrop-filter backdrop-blur-lg flex items-center">
+        <div>
+          React template with tailwindcss
+        </div>
+      </div>
+      <div className="relative">
+        <div className="rounded-full h-64 w-64 absolute top-1/2 -right-12 bg-gradient-to-br from-white to-transparent z-0"></div>
+        <div className="rounded-full h-40 w-40 absolute -top-52 right-96 bg-gradient-to-tl from-white to-transparent z-0"></div>
+      </div> */}
     </div>
   );
 };
